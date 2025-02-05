a6a1c275720d383fe0f77e0bf00ee047afd5a0cf
Ramasubramaniyan2003 <117003403+Ramasubramaniyan2003@users.noreply.github.com>
2025-02-05 23:19:31 +0530

    app minor changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const https = require('https');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoute');
@@ -17,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Connect to MongoDB
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
@@ -41,13 +43,15 @@ app.get('/', (req, res) => {
 });
 
 // Redirect HTTP to HTTPS (only in production)
-
 if (process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.protocol !== 'https') {
-      return res.redirect(301, 'https://' + req.headers.host + req.url);
-    }
-    next();
+  const http = require('http');
+  http.createServer((req, res) => {
+    res.redirect(301, 'https://' + req.headers.host + req.url); // Redirect HTTP to HTTPS
+  }).listen(80); // HTTP port
+
+  // HTTPS server
+  https.createServer(app).listen(443, () => {
+    console.log('HTTPS server running on port 443');
   });
 }
 
