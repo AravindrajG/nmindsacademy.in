8bfcaae2e25b21d8c7fccabb42a6e472613ac0f8
aravindraj <garuvir@gmail.com>
2025-05-06 18:05:03 +0530

    First commit

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 // Configuration using your Cloudinary credentials
 cloudinary.config({
-  cloud_name: 'dblw2fpoh',  // Replace with your Cloudinary cloud name
-  api_key: '451272537281443',        // Replace with your API key
-  api_secret: '6TDUbGzF4GdW7PglwQ25_C5OMsE'   // Replace with your API secret
+  cloud_name: 'dbkiks5y4',  // Replace with your Cloudinary cloud name
+  api_key: '472566923421752',        // Replace with your API key
+  api_secret: 'TWBxYI77yHuCHJjxa1wN74Z2n5Y'   // Replace with your API secret
 });
 
 module.exports = cloudinary;
